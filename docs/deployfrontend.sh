d0dd6ea6bbc1bffa520721994fb38a3997d67a62
nguyen <hiep.nguyenvan1@ncc.asia>
2022-11-20 10:54:05 +0700

    Adding frontend files to Github Pages

--- docs/deployfrontend.sh
+++ docs/deployfrontend.sh
@@ -1,5 +1,5 @@
-rsync -r src/ docs/
-rsync build/contracts/ChainList.json docs/
+cp -i src/ docs/
+cp -i build/contracts/ChainList.json docs/
 git add .
 git commit -m "Adding frontend files to Github Pages"
 git push
